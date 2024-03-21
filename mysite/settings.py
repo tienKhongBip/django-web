d56d71d90c738e91ecb363922b87d56d1c54786c
tienKhongBip <121567436+tienKhongBip@users.noreply.github.com>
2024-03-22 01:02:58 +0700

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,6 +149,6 @@ CKEDITOR_CONFIGS = {
         'toolbar': None,
     },
 }
-
+django_heroku.settings(locals())
 
 ###################################
