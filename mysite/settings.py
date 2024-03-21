6e0365b7041ffe321d834316de2e2ae108b31138
tienKhongBip <2154810090@vaa.edu.vn>
2024-03-21 22:22:45 +0700

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,6 +149,5 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-django_heroku.settings(locals())
 
 ###################################
